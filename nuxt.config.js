716fe4349de90a3f1de49082d5a0a02ec80ab06f
freddance <70547931+freddance@users.noreply.github.com>
2021-06-02 23:12:59 +0100

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,6 +81,9 @@ export default {
                 src: '/sb-admin-2.js',
             },
             {
+              src: 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js'
+            },
+            {
                 src: '/sb-admin-2.min.js',
             }
 
