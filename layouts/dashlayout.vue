e5c2c64595759f01f6375f921d562887c1112504
freddance <70547931+freddance@users.noreply.github.com>
2021-07-03 13:46:06 +0100

    new

--- layouts/dashlayout.vue
+++ layouts/dashlayout.vue
@@ -122,50 +122,16 @@
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <nuxt-link class="nav-link" to="/admin/dashboard">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></nuxt-link>
-            </li>
+         
 
             <!-- Divider -->
 
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Industries</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Finace</h6>
-                        <nuxt-link class="collapse-item" to="/admin/industry/_name">Fashion</nuxt-link>
-                        <nuxt-link class="collapse-item" to="/admin/delltechnology">Technology</nuxt-link>
-                        <nuxt-link class="collapse-item" to="/admin/economy">Economy</nuxt-link>
-                    </div>
-                </div>
-            </li>
+
 
             <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Settings</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <nuxt-link class="collapse-item" to="/admin/email-setting">email setting</nuxt-link>
-                        <nuxt-link class="collapse-item" to="/admin/review-setting">review-setting</nuxt-link>
-                        <nuxt-link class="collapse-item" to="/admin/chartjs">Chart</nuxt-link>
-                        <nuxt-link class="collapse-item" to="/admin/ad,d-rating">Add rating</nuxt-link>
-                    </div>
-                </div>
-            </li>
+
 
             <!-- Divider -->
             <!-- <hr class="sidebar-divider">
