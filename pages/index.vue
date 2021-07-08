01e9c1a411011ec1521bc251838c4973ad781653
freddance <70547931+freddance@users.noreply.github.com>
2021-07-08 01:21:11 +0100

    new

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <b-row class="">
         <div class="spacer"></div>
         <b-col md="6" class="my-sm-5 mt-lg-5 pt-lg-5 order-sm-1 order-2 pr-md-0">
-          <h1 class="hero mt-lg-5 pt-lg-3 small-hero-text hero-text">
+          <h1 class="hero mt-lg-2 pt-lg-3 small-hero-text hero-text">
             Explore what
             <span style="color: #e57718" class="font-weight-bold">Nigerians</span>
             <br class="d-none d-md-block" />
