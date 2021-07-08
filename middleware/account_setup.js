cc3bc59bf6de965f61a3931f220699747ef66412
freddance <70547931+freddance@users.noreply.github.com>
2021-07-08 11:09:28 +0100

    Update account_setup.js

--- middleware/account_setup.js
+++ middleware/account_setup.js
@@ -1,6 +1,5 @@
 export default async function({ route, context, redirect, $auth, $axios, store, dispatch }) {
-    if ($auth.user && $auth.user.Picture == null) {
-        console.log('yes', $auth.user)
+    if ($auth.user && $auth.user.Sex === null) {
         return redirect('/setup-account')
     }
     if ($auth.user.isAdmin) {
